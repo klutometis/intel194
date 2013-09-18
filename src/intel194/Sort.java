a19a371c0bf42cfd7a24e5a4f9c10043355cca2f
Peter Danenberg <pcd@roxygen.org>
2013-09-17 23:52:11 -0700

    Jedi sort with variable length strings.

--- src/intel194/Sort.java
+++ src/intel194/Sort.java
@@ -1,16 +1,27 @@
 package intel194;
 
-import java.util.Random;
-import org.apache.commons.lang3.RandomStringUtils;
+import static jedi.functional.FunctionalPrimitives.listTabulate;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static jedi.functional.Comparables.sort;
 
-class Sort {
+import java.util.Random;
+import jedi.functors.DynamicFunctor;
+
+public class Sort {
     static Random random = new Random();
+    static int MIN_LENGTH = 5;
+    static int MAX_LENGTH = 10;
+    static int STRINGS = 100;
 
     static int random(int min, int max) {
         return random.nextInt(max - min) + min;
     }
 
+    public static String randomString() {
+        return randomAlphanumeric(random(MIN_LENGTH, MAX_LENGTH));
+    }
+
     public static void main(String[] args) {
-        System.out.println(RandomStringUtils.randomAlphanumeric(random(5, 10)));
+        System.out.println(sort(listTabulate(100, new DynamicFunctor(Sort.class, "randomString"))));
     }
 }
