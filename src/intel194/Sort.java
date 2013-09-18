ba5787b5fff4ae247bd158b0de09d12d4875660c
Peter Danenberg <pcd@roxygen.org>
2013-09-17 23:54:36 -0700

    Simplify to use constant-length strings.

--- src/intel194/Sort.java
+++ src/intel194/Sort.java
@@ -1,27 +1,20 @@
 package intel194;
 
 import static jedi.functional.FunctionalPrimitives.listTabulate;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static jedi.functional.Comparables.sort;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
-import java.util.Random;
 import jedi.functors.DynamicFunctor;
 
 public class Sort {
-    static Random random = new Random();
-    static int MIN_LENGTH = 5;
-    static int MAX_LENGTH = 10;
+    static int LENGTH = 5;
     static int STRINGS = 100;
 
-    static int random(int min, int max) {
-        return random.nextInt(max - min) + min;
-    }
-
     public static String randomString() {
-        return randomAlphanumeric(random(MIN_LENGTH, MAX_LENGTH));
+        return randomAlphabetic(LENGTH);
     }
 
     public static void main(String[] args) {
-        System.out.println(sort(listTabulate(100, new DynamicFunctor(Sort.class, "randomString"))));
+        System.out.println(sort(listTabulate(STRINGS, new DynamicFunctor(Sort.class, "randomString"))));
     }
 }
