208a4e643c255236acc99315ebeb1875560cf262
Peter Danenberg <pcd@roxygen.org>
2013-09-17 23:51:50 -0700

    Use jedi.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.functionaljava/functionaljava "3.1"]
-                 [org.apache.commons/commons-lang3 "3.1"]]
+  :dependencies [[org.apache.commons/commons-lang3 "3.1"]
+                 [org.codehaus.jedi/jedi-core "3.0.5"]]
   :java-source-paths ["src"]
   ;; So things like `mvn exec:java -Dexec.mainClass=intel194.Hello'
   ;; will work; deposits pom.xml in the current dir.
