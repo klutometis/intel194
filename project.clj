af1cb4dc6041c15a7819c13405fe62cbeac78ed9
Peter Danenberg <pcd@roxygen.org>
2013-09-17 17:02:23 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject intel194 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.functionaljava/functionaljava "3.1"]
+                 [org.apache.commons/commons-lang3 "3.1"]]
+  :java-source-paths ["src"]
+  ;; So things like `mvn exec:java -Dexec.mainClass="intel194.Hello"'
+  ;; will work; deposits pom.xml in the current dir.
+  :target-path ""
+  :main intel194.Hello)
---
+++ src/intel194/Hello.java
@@ -0,0 +1,7 @@
+package intel194;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("harro");
+    }
+}
